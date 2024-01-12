083194010f37fc4cb1e7059cc599f62df8256877
isac7722 <isac7722@gmail.com>
2024-01-12 14:48:59 +0900

    study

--- BaekJoon/src/main/java/단계별로_풀어보기/BJ11브푸트포스/bj2231/bj2231.java
+++ BaekJoon/src/main/java/단계별로_풀어보기/BJ11브푸트포스/bj2231/bj2231.java
@@ -1,60 +1,92 @@
 package 단계별로_풀어보기.BJ11브푸트포스.bj2231;
 
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class bj2231 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();
-        int M = in.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
-        }
-
-        int result = search(arr, N, M);
-        System.out.println(result);
-    }
-
-
-    // 탐색
-    static int search(int[] arr, int N, int M) {
         int result = 0;
 
-        // 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
-        for (int i = 0; i < N - 2; i++) {
+        for (int i = 0; i < N; i++){
+            int number = i;
+            int sum = 0;
 
-            // 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
-            for (int j = i + 1; j < N - 1; j++) {
+            while (number != 0){
+                sum += number % 10;
+                number /= 10;
 
-                // 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
-                for (int k = j + 1; k < N; k++) {
+            }
 
-                    // 3개 카드의 합 변수 temp
-                    int temp = arr[i] + arr[j] + arr[k];
-
-                    // M과 세 카드의 합이 같다면 temp return 및 종료
-                    if (M == temp) {
-                        return temp;
-                    }
-
-                    // 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신
-                    if(result < temp && temp < M) {
-                        result = temp;
-                    }
-                }
+            if(sum + i == N){
+                result = i;
+                break;
             }
         }
 
-        // 모든 순회를 마치면 result 리턴
-        return result;
+        System.out.println(result);
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*public static void main(String[] args) {
+        a+b+c+(a*100 + b*10 + c) = D
+
+        Scanner scanner = new Scanner(System.in);
+
+        *//* 사용자에게 정수를 입력받는다 *//*
+        int number = scanner.nextInt();
+
+        *//* 입력 받은 정수가 몇자리 숫자 인지 확인 한다 *//*
+        int digit = countDigits(number);
+
+        int result = 0;
+
+        for (int i = 0; i < digit; i++) {
+
+
+            if (number == result){
+                System.out.println(result);
+                return;
+            }
+        }
+        System.out.println(result);
+
     }
 
-}
+
+
+
+
+    public static int countDigits (int number){
+        //숫자가 0인 경우 1자리로 처리
+        if (number == 0){
+            return 1;
+        }
+
+        int count = 0;
+        while (number > 0){
+            number /= 10;
+            count++;
+        }
+        return count;
+    }*/
+
+
 
 
